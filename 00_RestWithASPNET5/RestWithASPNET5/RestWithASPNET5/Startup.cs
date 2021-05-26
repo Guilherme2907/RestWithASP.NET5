c90187ae4a2473b18d02bfca08697df94960cb4d
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2021-05-26 00:11:19 -0300

    Criação das migrations e seeds para as tabelas de pessoas e livros

--- 00_RestWithASPNET5/RestWithASPNET5/RestWithASPNET5/Startup.cs
+++ 00_RestWithASPNET5/RestWithASPNET5/RestWithASPNET5/Startup.cs
@@ -1,32 +1,54 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using RestWithASPNET5.Models.Context;
+using RestWithASPNET5.Repository;
+using RestWithASPNET5.Repository.Implementations;
+using RestWithASPNET5.Services;
+using RestWithASPNET5.Services.Implementations;
+using Serilog;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace RestWithASPNET5
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public IWebHostEnvironment Environment { get; }
 
         public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration,IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+
+            var connection = Configuration["MySQLConnection:MySQLConnectionString"];
+
+            services.AddDbContext<MySqlContext>(options => options.UseMySql(connection,ServerVersion.AutoDetect(connection)));
+
+            if (Environment.IsDevelopment()) MigrateDatabase(connection);
+
+            services.AddApiVersioning();
+
+            //Services
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
+            services.AddScoped<IBookService, BookServiceImplementation>();
+
+            //Repositories
+            services.AddScoped<IPersonRepository, PersonRepositoryImplementation>();
+            services.AddScoped<IBookRepository, BookRepositoryImplementation>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +69,26 @@ namespace RestWithASPNET5
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private void MigrateDatabase(string connection)
+        {
+            try
+            {
+                var envolveConnection = new MySql.Data.MySqlClient.MySqlConnection(connection);
+                var evolve = new Evolve.Evolve(envolveConnection, msg => Log.Information(msg))
+                {
+                    Locations = new List<string> { "db/migrations", "db/dataset" },
+                    IsEraseDisabled = true
+                };
+                evolve.Migrate();
+            }
+            catch (Exception ex)
+            {
+
+                Log.Error("Database migration failed", ex);
+                throw;
+            }
         }
     }
 }
---
+++ 00_RestWithASPNET5/RestWithASPNET5/RestWithASPNET5/db/dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO `person` (`id`, `address`, `first_name`, `last_name`, `gender`) VALUES
+	(1, 'São Paulo - Brasil', 'Guilherme', 'Magalhaes', 'Male'),
+	(3, 'São Paulo - Brasil', 'Ana', 'Silva', 'Female');
