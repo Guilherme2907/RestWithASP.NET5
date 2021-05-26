98593be3546c00434b0f3d0e5df39378d4ab01cf
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2021-05-26 00:14:54 -0300

    Início da implementação do GenericRepository na api

---
+++ 00_RestWithASPNET5/RestWithASPNET5/RestWithASPNET5/Repository/IPersonRepository.cs
@@ -0,0 +1,15 @@
+﻿using RestWithASPNET5.Models;
+using System.Collections.Generic;
+
+namespace RestWithASPNET5.Repository
+{
+    public interface IPersonRepository
+    {
+        Person Create(Person person);
+        Person Update(Person person);
+        Person FindById(long id);
+        void Delete(long id);
+        List<Person> FindAll();
+        bool Exists(long id);
+    }
+}
